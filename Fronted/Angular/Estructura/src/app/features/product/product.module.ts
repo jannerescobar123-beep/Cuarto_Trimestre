befccb6e2aadc801a13afd2b3a3be22a1ba62fe9
Janner <jannerescobar123@gmail.com>
2026-05-22 15:01:14 -0500

    feat:Se inicia nuevo tema de estructuras y modulos para aplicar componentes

---
+++ Fronted/Angular/Estructura/src/app/features/product/product.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductListComponent } from './page/product-list/product-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductCardComponent,
+    ProductListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductModule { }
---
+++ Fronted/Angular/Estructura/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PipeComponent } from './components/pipe/pipe.component';
+
+
+
+@NgModule({
+  declarations: [
+    PipeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
